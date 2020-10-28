eb58a76bed9edb0c5bca5e128ef9ff2f84e5d090
Dhanu K <dhanu@returntrucks.com>
2020-10-28 21:38:58 +0530

    URL fix

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ We have a bit lengthy process to do simple things like creating calendar entries
 Also, the lack of a chrome extension for this simple task inspired me to take this project.
 
 
-
 ## What it does
 
 Smart Calendar automatically -
@@ -16,7 +15,6 @@ Smart Calendar automatically -
 - **Creates calendar entries** by scanning through the active webpage.
 - Able to **identifies** dates, subject, time, and ~~**appropriate time zones**~~ through intelligence.
 - Also, **updates relevant fields in the calendar.**
-
 
 
 ## How I built it
@@ -28,12 +26,11 @@ Smart Calendar automatically -
 - Shows the calendar in edit mode to confirm and save the event.
 
 
-
 ## Challenges I ran into
 
 - Various websites use different date and time formats and correspond to distinct time zones.
 - The main challenge is, **writing regular expressions** for the Intelligence to understand various date and time formats.
-- **Processing the information locally within the browser** and not sending any user information to the server, thereby **ensuring  user privacy.**
+- **Processing the information locally within the browser** and not sending any user information to the server, thereby **ensuring user privacy.**
 
 
 ## How to install this chrome extension
@@ -44,3 +41,17 @@ Smart Calendar automatically -
  - Click on **Load unpacked** button in the **top left corner**
  - Open any website in the chrome, then open **Smart Calander** Extension and click **Execute OCR** button
  - That's it.
+
+
+## What I learned
+
+ - Optical character recognition(**OCR**) using **Tesseract** library
+ - How to take a screenshot with chrome extension
+ - Enhancing the screenshot using various Machine Learning(**ML**) techniques
+ - Identifying different date formats from complex strings using Machine Learning(**ML**) methods with confidence scores
+
+
+## What's next for Smart Calendar
+
+- Implementing for various timezones and complex scenarios 
+- Making it freely available to the public through Chrome extension apps
--- js/main.js
+++ js/main.js
@@ -20,70 +20,70 @@ var formates = [
 ]
 var title;
 var html;
-var url;
+var activeTabURL;
 var extractedText;
 var eventDates = [];
 var outputFormat = 'DD/MM/YYYY hh:mm A';
 $(document).ready(function(){
-    $("#start-btn").click(function(){
-        chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-            let image = document.getElementById('image');
-            image.src = screenshotUrl
-            image.crossOrigin = "Anonymous";
+    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+        let image = document.getElementById('image');
+        image.src = screenshotUrl
+        image.crossOrigin = "Anonymous";
 
-            const startOCR = async () => {
-                //const image = document.getElementById('image');
-                const result = document.getElementById('result');
-                const { createWorker } = Tesseract;
-                const worker = createWorker({
-                    workerPath: chrome.runtime.getURL('js/worker.min.js'),
-                    langPath: chrome.runtime.getURL('traineddata'),
-                    corePath: chrome.runtime.getURL('js/tesseract-core.wasm.js'),
-                    logger: m => {
-                        $("#result").show();
-                        $("#result").find("#progress").val(m.progress);
-                        $("#result").find("#message").html(m.status);
-                    }
+        const startOCR = async () => {
+            //const image = document.getElementById('image');
+            const result = document.getElementById('result');
+            const { createWorker } = Tesseract;
+            const worker = createWorker({
+                workerPath: chrome.runtime.getURL('js/worker.min.js'),
+                langPath: chrome.runtime.getURL('traineddata'),
+                corePath: chrome.runtime.getURL('js/tesseract-core.wasm.js'),
+                logger: m => {
+                    $("#result").show();
+                    $("#result").find("#progress").val(m.progress);
+                    $("#result").find("#message").html(m.status);
+                }
+            });
+          
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            const { data: { text } } = await worker.recognize(image);
+            console.log(text)
+            $("#result").hide();
+            var ocrText = text;
+            extractedText = text;
+            $.each(expressions,function(index,expression){
+                var res =ocrText.match(expression);
+                if(res){
+                    $.each(res,function(i,val){
+                        ocrText = ocrText.replace(val,"");
+                        eventDates.push(convertToDate(formates[index],val));
+                    });
+                }
+            });
+
+            $("#output").show();
+            $("#subject").html(title)
+            $.each(eventDates,function(i,date){
+                var eventType = "Full Day";
+                if(date.length > 1){
+                    eventType = "Regular";
+                }
+                $("table tbody").append("<tr><td>"+date.join(" to ")+"</td><td><a target='_blank' href='#' id='createEventTag' data-index='"+i+"'>Create Event</a></td></tr>");
+                $("#createEventTag[data-index='"+i+"']").click(function(){
+                    chrome.tabs.create({ url: openCreateEvent(i) });
                 });
-              
-                await worker.load();
-                await worker.loadLanguage('eng');
-                await worker.initialize('eng');
-                const { data: { text } } = await worker.recognize(image);
-                console.log(text)
-                $("#result").hide();
-                var ocrText = text;
-                extractedText = text;
-                $.each(expressions,function(index,expression){
-                    var res =ocrText.match(expression);
-                    if(res){
-                        $.each(res,function(i,val){
-                            ocrText = ocrText.replace(val,"");
-                            eventDates.push(convertToDate(formates[index],val));
-                        });
-                    }
-                });
+            });
+            await worker.terminate();
+        }
 
-                $("#output").show();
-                $("#subject").html(title)
-                $.each(eventDates,function(i,date){
-                    var eventType = "Full Day";
-                    if(date.length > 1){
-                        eventType = "Regular";
-                    }
-                    $("table tbody").append("<tr><td>"+date.join(" to ")+"</td><td><a target='_blank' href='"+openCreateEvent(i)+"'>Create Event</a></td></tr>");
-                });
-                await worker.terminate();
-            }
-
-            image.onload = startOCR
-        });
-
+        image.onload = startOCR
     });
 });
 chrome.tabs.query({active: true}, function(tabs) {
     var tab = tabs[0];
-    url = tab.url;
+    activeTabURL = tab.url;
     title = tab.title;
     console.log(tab)
     chrome.tabs.executeScript(tab.id, {
@@ -98,17 +98,17 @@ function convertToDate(format,date){
     if(format[1]){
         return [moment(date,format[0]).format(outputFormat)];
     }else{
-        if(format[2] == 1 || format[2] == 3){
-            var stdFormat = 'MMMM DD,YYYY'
-            var parts = date.split("/[\s,]+/");
-            var from = parts[0]+" "+parts[1]+","+parts[3];
-            var to = parts[0]+" "+parts[2]+","+parts[3];
+        if(format[2] == 1){
+            var stdFormat = 'MMMM DD,YYYY hh:mm A'
+            var parts = date.split(/\W+/);
+            var from = parts[0]+" "+parts[1]+","+parts[3]+" 80:00 AM";
+            var to = parts[0]+" "+parts[2]+","+parts[3]+" 05:00 PM";
             return [moment(from,stdFormat).format(outputFormat),moment(to,stdFormat).format(outputFormat)]
-        }else if(format[2] == 2){
-            var stdFormat = 'MMMM DD'
-            var parts = date.split("/[\s,]+/");
-            var from = parts[0]+" "+parts[1];
-            var to = parts[0]+" "+parts[2];
+        }else if(format[2] == 2 || format[2] == 3){
+            var stdFormat = 'MMMM DD hh:mm A'
+            var parts = date.split(/\W+/);
+            var from = parts[0]+" "+parts[1]+" 08:00 AM";
+            var to = parts[0]+" "+parts[2]+" 05:00 PM";
             return [moment(from,stdFormat).format(outputFormat),moment(to,stdFormat).format(outputFormat)]
         }
     }
@@ -128,20 +128,20 @@ function getSubject(para,word){
 
 function openCreateEvent(i){
     var dates = eventDates[i];
-    var details = "URL:" + url;
+    var details = "URL:" + activeTabURL;
     var tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var url = "https://www.google.com/calendar/render?action=TEMPLATE&text="+encodeURIComponent(title);
     if(dates.length == 1){
-        var from = moment(dates[0],outputFormat).format("YYYYMMDDThhmmss");
-        var to = moment(dates[0],outputFormat).add(1, 'hours').format("YYYYMMDDThhmmss");
-        return "https://www.google.com/calendar/render?action=TEMPLATE&text="+title+"&details="+details+"&dates="+from+"/"+to+"&ctz="+tz;
+        var from = moment(dates[0],outputFormat).format("YYYYMMDDTHHmmss");
+        var to = moment(dates[0],outputFormat).add(1, 'hours').format("YYYYMMDDTHHmmss");
+        url = "https://www.google.com/calendar/render?action=TEMPLATE&text="+encodeURIComponent(title)+"&details="+details+"&dates="+from+"/"+to+"&ctz="+tz;
     }else if(dates.length == 2){
-        var from = moment(dates[0],outputFormat).format("YYYYMMDDThhmmss");
-        var to = moment(dates[1],outputFormat).format("YYYYMMDDThhmmss");
-        return "https://www.google.com/calendar/render?action=TEMPLATE&text="+title+"&details="+details+"&dates="+from+"/"+to+"&ctz="+tz;
-    }else{
-        return "https://www.google.com/calendar/render?action=TEMPLATE&text="+title;
+        var from = moment(dates[0],outputFormat).format("YYYYMMDDTHHmmss");
+        var to = moment(dates[1],outputFormat).format("YYYYMMDDTHHmmss");
+        url = "https://www.google.com/calendar/render?action=TEMPLATE&text="+encodeURIComponent(title)+"&details="+details+"&dates="+from+"/"+to+"&ctz="+tz;
     }
-
+    console.log(url);
+    return (url);
 }
 function openURL(newURL){
     chrome.browserAction.onClicked.addListener(function(activeTab){
